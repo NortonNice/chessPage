5f8c5e08f47bcad14386f3c2870ec2929d8bfcd6
Norton Nice <nortonnice@gmail.com>
2019-02-27 14:37:25 -0800

    styling and text

--- src/components/Alekhine/Alekhine.js
+++ src/components/Alekhine/Alekhine.js
@@ -6,11 +6,12 @@ function Alekhine() {
     return (
         <div className='alekhineCard'>
         <img className='alekhinePhoto' src={alex} alt="alexander"/>
-        <p>Spicy jalapeno bacon ipsum dolor amet turkey kevin t-bone ham hock nisi eu.
-         Commodo veniam jowl consequat, id culpa kielbasa shank. Cow bresaola aute ribeye
-          t-bone, flank ut nulla et corned beef ad commodo laboris aliqua porchetta. 
-          Buffalo picanha ad, short ribs eiusmod ham incididunt adipisicing jowl cupim.
-           Jerky ball tip ex ground round.</p>
+        <p className="cardText">Born in Moscow in 1892, Alexander Alekhine has long been considered
+            to be one of the greatest chess players of all time. He became
+            world chess champion in 1927 by defeating the legendary Jose Raul Capablanca.
+            Alekhine lost this title in 1935, and then regained it in 1937. Alexander 
+            Alekhine passed away in 1946, and remains the only World Chess Champion 
+            to die while holding the title.</p>
         </div>
     )
 }
